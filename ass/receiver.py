1f03cc755e825ca5c374527169616a3bb1ea8db7
Anthony Zhong <anthonyzhong96@gmail.com>
2018-10-14 17:06:02 +1100

    Make receiver methods private.

--- ass/receiver.py
+++ ass/receiver.py
@@ -19,13 +19,13 @@ class Receiver:
         print('Listening for connection...')
         self.state = State.LISTEN
         while True:
-            message, address = receiver.receive()
+            message, address = receiver._receive()
             header = decode(message).header
             if self.state == State.LISTEN and header.syn:
                 self.state = State.SYN_RCVD
                 self.ackNum = 1
                 responseHeader = Header(ackNum=self.ackNum, ack=True, syn=True) # SYN+ACK
-                receiver.send(address=address, header=responseHeader)
+                receiver._send(address=address, header=responseHeader)
                 print('Sent SYN+ACK')
             elif self.state == State.SYN_RCVD and header.ack:
                 self.state = State.ESTABLISHED
@@ -35,20 +35,20 @@ class Receiver:
 
     def receiveFile(self):
         while True:
-            message, address = receiver.receive()
+            message, address = receiver._receive()
             message = decode(message)
             header = message.header
             print('Received message. Message Seq Num: {seqNum} Receiver Ack Num: {ackNum}'.format(seqNum=message.header.seqNum, ackNum=receiver.ackNum))
             if header.seqNum == self.ackNum:
-                self.write(message.payload)
+                self._write(message.payload)
                 responseHeader = Header(ackNum=self.ackNum, ack=True)
             elif header.seqNum > self.ackNum:
-                self.addToBuffer(message)
+                self._addToBuffer(message)
                 responseHeader = Header(ackNum=self.ackNum, ack=True)
             elif header.fin:
                 self.teardown()
 
-            receiver.send(address=address, header=responseHeader)
+            receiver._send(address=address, header=responseHeader)
 
     def teardown(self):
         while True:
@@ -66,10 +66,10 @@ class Receiver:
                 return
 
 
-    def receive(self):
+    def _receive(self):
         return self.socket.recvfrom(4096)
 
-    def send(self, address, header=None, payload=None):
+    def _send(self, address, header=None, payload=None):
         if not header:
             return
         if payload:
@@ -80,7 +80,7 @@ class Receiver:
 
         self.socket.sendto(message.encode(), address)
 
-    def write(self, payload):
+    def _write(self, payload):
         print('Writing to file...')
         if self.ackNum == 1:
             with open(self.filename, 'w') as f:
@@ -90,7 +90,7 @@ class Receiver:
                 f.write(payload)
         self.ackNum += len(payload)
 
-    def addToBuffer(self, message):
+    def _addToBuffer(self, message):
         print('Adding to buffer...')
 
 if __name__ == '__main__':
