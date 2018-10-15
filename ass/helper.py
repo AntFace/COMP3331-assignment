a96926232416c64890891949d395d029e4cafb6b
Anthony Zhong <anthonyzhong96@gmail.com>
2018-10-15 13:26:50 +1100

    Remove MWS and MSS from Header.

--- ass/helper.py
+++ ass/helper.py
@@ -14,12 +14,10 @@ class State(Enum):
     LAST_ACK = 9
 
 class Header:
-    def __init__(self, seqNum=0, ackNum=0, checksum=0, mws=0, mss=0, ack=False, syn=False, fin=False):
+    def __init__(self, seqNum=0, ackNum=0, checksum=0, ack=False, syn=False, fin=False):
         self.seqNum = seqNum
         self.ackNum = ackNum
         self.checksum = checksum
-        self.mws = mws
-        self.mss = mss
         self.ack = ack
         self.syn = syn
         self.fin = fin
@@ -28,8 +26,6 @@ class Header:
         bits = '{0:032b}'.format(self.seqNum)
         bits += '{0:032b}'.format(self.ackNum)
         bits += '{0:016b}'.format(self.checksum)
-        bits += '{0:032b}'.format(self.mws)
-        bits += '{0:032b}'.format(self.mss)
         bits += '{0:01b}'.format(self.ack)
         bits += '{0:01b}'.format(self.syn)
         bits += '{0:01b}'.format(self.fin)
@@ -58,16 +54,14 @@ def decode(encodedMessage):
     seqNum = int(encodedMessage[0:32], 2)
     ackNum = int(encodedMessage[32:64], 2)
     checksum = int(encodedMessage[64:80], 2)
-    mws = int(encodedMessage[80:112], 2)
-    mss = int(encodedMessage[112:144], 2)
-    ack = int(encodedMessage[144:145], 2)
-    syn = int(encodedMessage[145:146], 2)
-    fin = int(encodedMessage[146:147], 2)
+    ack = int(encodedMessage[80:81], 2)
+    syn = int(encodedMessage[81:82], 2)
+    fin = int(encodedMessage[82:83], 2)
 
-    header = Header(seqNum, ackNum, checksum, mws, mss, ack, syn, fin)
+    header = Header(seqNum, ackNum, checksum, ack, syn, fin)
 
-    if len(encodedMessage) > 146:
-        payload = encodedMessage[147:].decode('iso-8859-1')
+    if len(encodedMessage) > 82:
+        payload = encodedMessage[83:].decode('iso-8859-1')
         
         return Message(header, payload)
     else:
