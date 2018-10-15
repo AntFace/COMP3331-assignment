0c033ba8a23dfcf98958bdcc7148433d28a3c8f1
Anthony Zhong <anthonyzhong96@gmail.com>
2018-10-15 16:16:17 +1100

    Skeleton PLD module.

---
+++ ass/pld.py
@@ -0,0 +1,10 @@
+class PLD:
+    def __init__(self, pDrop, pDuplicate, pCorrupt, pOrder, maxOrder, pDelay, maxDelay, seed):
+        self.pDrop = pDrop
+        self.pDuplicate = pDuplicate
+        self.pCorrupt = pCorrupt
+        self.pOrder = pOrder
+        self.maxOrder = maxOrder
+        self.pDelay = pDelay
+        self.maxDelay = maxDelay
+        self.seed = seed
