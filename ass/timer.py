5d9df2c1b8feafb34e686116a606065a3b6f4987
Anthony Zhong <anthonyzhong96@gmail.com>
2018-10-16 13:02:56 +1100

    Timer has start and stop functions to generate sampleRTTs for updating.

--- ass/timer.py
+++ ass/timer.py
@@ -1,3 +1,5 @@
+import time
+
 class Timer:
     def __init__(self, estimatedRTT=0.5, devRTT=0.25, gamma=4):
         self.estimatedRTT = estimatedRTT
@@ -6,5 +8,25 @@ class Timer:
         self.beta = 0.25
         self.gamma = gamma
 
+        self.startTime = None
+
+    @property
+    def isRunning(self):
+        return self.startTime is not None
+
+    def start(self):
+        self.startTime = time.time()
+
+    def stop(self):
+        self._update(time.time() - self.startTime)
+        self.startTime = None
+
+    def discard(self):
+        self.startTime = None
+
     def getTimeoutInterval(self):
         return self.estimatedRTT + self.gamma * self.devRTT
+
+    def _update(self, sampleRTT):
+        self.estimatedRTT = (1 - self.alpha) * self.estimatedRTT + self.alpha * self.estimatedRTT
+        self.devRTT = (1 - self.beta) * self.devRTT + self.beta * abs(sampleRTT - self.estimatedRTT)
